659a85131ca4d4f05a06aa2125a89327c82342d0
mouse <m1689141525@gmail.com>
2022-07-07 22:14:45 +0800

    refactor ComedyCalculator

--- src/main/java/org/coderead/calculator/ComedyCalculator.java
+++ src/main/java/org/coderead/calculator/ComedyCalculator.java
@@ -10,18 +10,15 @@ public class ComedyCalculator extends AbstractPerformanceCalculator {
     public double getVolumeCredits(Performance performance) {
         int max = Math.max(performance.getAudience() - 30, 0);
         double floor = Math.floor(performance.getAudience() / 5);
-        double volumeCredits1 = max + floor;
-        return volumeCredits1;
+        return max + floor;
     }
 
     @Override
     public double getAmount(Performance performance) {
-        int thisAmount;
-        thisAmount = 30000;
+        int thisAmount = 30000;
         if (performance.getAudience() > 20) {
-            thisAmount += 10000 + 500 * (performance.getAudience() - 20);
+            thisAmount = thisAmount + (10000 + 500 * (performance.getAudience() - 20));
         }
-        thisAmount += 300 * performance.getAudience();
-        return thisAmount;
+        return thisAmount + 300 * performance.getAudience();
     }
 }
