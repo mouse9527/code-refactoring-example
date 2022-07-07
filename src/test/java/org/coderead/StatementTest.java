d8084a13aa057ee87c8a56808e9223b25157248d
mouse <m1689141525@gmail.com>
2022-07-07 21:29:36 +0800

    refactor test

--- src/test/java/org/coderead/StatementTest.java
+++ src/test/java/org/coderead/StatementTest.java
@@ -17,38 +17,46 @@ import java.util.Map;
  */
 public class StatementTest {
 
+    private static final String EXPECTED = "Statement for BigCo Hamlet: $650.00 (55 seats)\n" +
+            " As You Like It: $580.00 (35 seats)\n" +
+            " Othello: $500.00 (40 seats)\n" +
+            " Ji Xie Shi: $500.00 (40 seats)\n" +
+            " Bai She Zhuan: $500.00 (40 seats)\n" +
+            "Amount owed is $2,730.00\n" +
+            "You earned 67 credits\n";
+    private static final String PLAYS_JSON = "{" +
+            "\"hamlet\":{\"name\":\"Hamlet\",\"type\":\"tragedy\"}," +
+            "\"as-like\":{\"name\":\"As You Like It\",\"type\":\"comedy\"}," +
+            "\"othello\":{\"name\":\"Othello\",\"type\":\"tragedy\"}," +
+            "\"jxs\":{\"name\":\"Ji Xie Shi\",\"type\":\"action\"}," +
+            "\"bsz\":{\"name\":\"Bai She Zhuan\",\"type\":\"love\"}" +
+            "}";
+    private static final String INVOICES_JSON = "{" +
+            "\"customer\":\"BigCo\",\"performances\":[" +
+            "{\"playId\":\"hamlet\",\"audience\":55}" +
+            "{\"playId\":\"as-like\",\"audience\":35}" +
+            "{\"playId\":\"othello\",\"audience\":40}" +
+            "{\"playId\":\"jxs\",\"audience\":40}" +
+            "{\"playId\":\"bsz\",\"audience\":40}" +
+            "]" +
+            "}";
+
     @Test
     public void test() {
-        String expected = "Statement for BigCo Hamlet: $650.00 (55 seats)\n" +
-                " As You Like It: $580.00 (35 seats)\n" +
-                " Othello: $500.00 (40 seats)\n" +
-                " Ji Xie Shi: $500.00 (40 seats)\n" +
-                " Bai She Zhuan: $500.00 (40 seats)\n" +
-                "Amount owed is $2,730.00\n" +
-                "You earned 67 credits\n";
-        final String plays = "{" +
-                "\"hamlet\":{\"name\":\"Hamlet\",\"type\":\"tragedy\"}," +
-                "\"as-like\":{\"name\":\"As You Like It\",\"type\":\"comedy\"}," +
-                "\"othello\":{\"name\":\"Othello\",\"type\":\"tragedy\"}," +
-                "\"jxs\":{\"name\":\"Ji Xie Shi\",\"type\":\"action\"}," +
-                "\"bsz\":{\"name\":\"Bai She Zhuan\",\"type\":\"love\"}" +
-                "}";
+        Statement statement = new Statement(createInvoice(), createPlayMap());
 
-        final String invoices = "{" +
-                "\"customer\":\"BigCo\",\"performances\":[" +
-                "{\"playId\":\"hamlet\",\"audience\":55}" +
-                "{\"playId\":\"as-like\",\"audience\":35}" +
-                "{\"playId\":\"othello\",\"audience\":40}" +
-                "{\"playId\":\"jxs\",\"audience\":40}" +
-                "{\"playId\":\"bsz\",\"audience\":40}" +
-                "]" +
-                "}";
-
-        TypeReference<Map<String, Play>> typeReference = new TypeReference<Map<String, Play>>(){};
-        Map<String, Play> playMap = JSONObject.parseObject(plays, typeReference);
-        Invoice invoice = JSONObject.parseObject(invoices, Invoice.class);
-        Statement statement = new Statement(invoice, playMap);
         String result = statement.show();
-        Assert.assertEquals(expected, result);
+
+        Assert.assertEquals(EXPECTED, result);
+    }
+
+    private Invoice createInvoice() {
+        return JSONObject.parseObject(INVOICES_JSON, Invoice.class);
+    }
+
+    private Map<String, Play> createPlayMap() {
+        TypeReference<Map<String, Play>> typeReference = new TypeReference<Map<String, Play>>() {
+        };
+        return JSONObject.parseObject(PLAYS_JSON, typeReference);
     }
 }
